fd844c94ef27e475e6be5dd4f2645d2552c88d2a
Mehdi <mhdsaffari@gmail.com>
2025-07-18 17:02:10 +0330

    Delete interface directory

--- interface/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-import interface.models
-
-# Register models
-admin.site.register(interface.models.TelegramUser)
-
-# Optional: Add custom admin functionality for specific models
-class mainAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "completed")
